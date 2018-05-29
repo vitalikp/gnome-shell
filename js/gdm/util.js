58c1b0de500b049f049bf972dd4f57df9618b97e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-05-29 00:00:04 +0000

    authPrompt: Do not enable sensitivity if retries are disallowed
    Set the sensitivity of the UI according to the canRetry parameter and thus
    if no more logins are allowed don't take any input.
    
    Fixes #311
    
    (cherry picked from commit 8f848925f6cdae0d96c355faa8acc2b0d45ab16c)

--- js/gdm/util.js
+++ js/gdm/util.js
@@ -518,7 +518,7 @@ var ShellUserVerifier = new Lang.Class({
             }
         }
 
-        this.emit('verification-failed');
+        this.emit('verification-failed', canRetry);
     },
 
     _onConversationStopped(client, serviceName) {
