dae68e9d4772a1f8d60dd81d97e935db3ceba60a
Florian Müllner <fmuellner@gnome.org>
2019-01-29 20:20:09 +0100

    style: Avoid trailing commas in array destructuring
    When destructuring multiple return values, we often use trailing commas
    to indicate that there are additional elements that we are ignoring.
    
    There isn't anything inherently wrong with that, but it's a style that's
    too confusing for eslint - on the one hand we require a space after a
    comma, on the other hand we require no space before closing brackets.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/607

--- js/ui/status/keyboard.js
+++ js/ui/status/keyboard.js
@@ -419,7 +419,7 @@ var InputSourceManager = class {
             let exists = false;
 
             if (type == INPUT_SOURCE_TYPE_XKB) {
-                [exists, displayName, shortName, , ] =
+                [exists, displayName, shortName] =
                     this._xkbInfo.get_layout_info(id);
             }
 
@@ -430,7 +430,7 @@ var InputSourceManager = class {
         if (infosList.length == 0) {
             let type = INPUT_SOURCE_TYPE_XKB;
             let id = KeyboardManager.DEFAULT_LAYOUT;
-            let [ , displayName, shortName, , ] = this._xkbInfo.get_layout_info(id);
+            let [ , displayName, shortName] = this._xkbInfo.get_layout_info(id);
             infosList.push({ type: type, id: id, displayName: displayName, shortName: shortName });
         }
 
