069f845514482ce3fb271c617aebffe5036e330e
Florian Müllner <fmuellner@gnome.org>
2019-08-19 21:33:15 +0200

    cleanup: Use consistent style for ternary operator
    We are currently inconsistent whether to put the operators in front
    of the corresponding line or at the end of the preceding one. The
    most dominant style for now is to put condition and first branch on
    the same line, and then align the second branch:
    
      let foo = condition ? fooValue
                          : notFooValue;
    
    Unfortunately that's a style that eslint doesn't support, so to account
    for it, our legacy configuration currently plainly ignores all indentation
    in conditionals.
    
    In order to drop that exception and not let messed up indentation slip
    through, change all ternary operators to the non-legacy style.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/725

--- js/ui/sessionMode.js
+++ js/ui/sessionMode.js
@@ -92,11 +92,11 @@ const _modes = {
         isLocked: false,
         isPrimary: true,
         unlockDialog: imports.ui.unlockDialog.UnlockDialog,
-        components: Config.HAVE_NETWORKMANAGER ?
-                    ['networkAgent', 'polkitAgent', 'telepathyClient',
-                     'keyring', 'autorunManager'] :
-                    ['polkitAgent', 'telepathyClient',
-                     'keyring', 'autorunManager'],
+        components: Config.HAVE_NETWORKMANAGER
+            ? ['networkAgent', 'polkitAgent', 'telepathyClient',
+               'keyring', 'autorunManager']
+            : ['polkitAgent', 'telepathyClient',
+               'keyring', 'autorunManager'],
 
         panel: {
             left: ['activities', 'appMenu'],
