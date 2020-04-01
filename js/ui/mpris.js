fa1ba33221767f01bd97b4df106b8f5533352ad4
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2020-04-01 23:28:05 +0300

    * mpris: add “Stop” DBUS method to “MediaPlayer2.Player” interface;

--- js/ui/mpris.js
+++ js/ui/mpris.js
@@ -118,6 +118,10 @@ var MprisPlayer = class MprisPlayer {
         this._playerProxy.PlayPauseRemote();
     }
 
+    stop() {
+        this._playerProxy.StopRemote();
+    }
+
     get canGoNext() {
         return this._playerProxy.CanGoNext;
     }
